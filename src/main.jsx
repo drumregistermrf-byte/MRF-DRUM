37ffa9a0e8cec29f54e600d356f5686a4c23f2c5
MRF Drum Tracker <doncherian3@gmail.com>
2026-08-06 13:06:09 +0530

    Wire frontend to the shared backend API instead of localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./lib/storageShim";
+import "./lib/apiStorage";
 import "./index.css";
 import DrumTracker from "./DrumTracker.jsx";
 
